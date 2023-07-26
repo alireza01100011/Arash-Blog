d05f1bc55f3a5df8a0d9ae938136872c74e7e3a2
Alireza <armanparsi206@outlook.com>
2023-07-26 15:42:18 +0330

    Implementation of like and dislike in the database

--- mod_blog/models.py
+++ mod_blog/models.py
@@ -1,6 +1,20 @@
-from sqlalchemy import Column , Integer , String , Text , Table , ForeignKey 
+from sqlalchemy import Column , Integer , String , Text , Table , ForeignKey , DateTime
 from app import db 
 
+from datetime import datetime 
+
+liks = Table('liks' , db.metadata ,
+    Column('user_id' , Integer , ForeignKey('users.id' , ondelete='cascade')),
+    Column('post_id' , Integer , ForeignKey('posts.id' , ondelete='cascade')),
+    Column('time' , DateTime , default=datetime.now)
+)
+
+disliks = Table('disliks' , db.metadata ,
+    Column('user_id' , Integer , ForeignKey('users.id' , ondelete='cascade')),
+    Column('post_id' , Integer , ForeignKey('posts.id' , ondelete='cascade')),
+    Column('time' , DateTime , default=datetime.now)
+
+)
 posts_categories = Table( 'posts_categories' , db.metadata ,
     Column('post_id' , Integer , ForeignKey('posts.id' , ondelete='cascade')),
     Column('categories_id' , Integer , ForeignKey('categories.id' , ondelete='cascade'))
@@ -15,17 +29,17 @@ class Post(db.Model):
     slug = Column(String(128) , nullable=False , unique=True)
     image = Column(Integer , nullable=True , unique=False)
     categories = db.relationship('Category' , secondary=posts_categories , back_populates='posts')
-    
+    users_liks = db.relationship('User' , secondary=liks , back_populates='posts_liked')
+    users_disliks = db.relationship('User' , secondary=disliks , back_populates='posts_disliked')
     def __repr__(self):
         return f'Post < {self.id} - {self.title[:24]} - {self.slug}> '
     
-    def __init__(self , title : str , content : str , summary : str , slug : str , image : int , categories : list):
+    def __init__(self , title : str , content : str , summary : str , slug : str , image : int ):
         self.title = title
         self.content = content
         self.summary = summary
         self.slug = slug
         self.image = image
-        self.categories = categories
 
 
 class Category(db.Model):
@@ -43,3 +57,22 @@ class Category(db.Model):
         self.name = name 
         self.description = description
         self.slug = slug
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer , primary_key=True)
+    full_name = Column(String(128) , nullable=False , unique=False)
+    email = Column(Integer , nullable=False , unique=True)
+    password = Column(String(128) , nullable=False , unique=False)
+    role = Column(Integer , nullable=False , unique=False)
+    posts_liked = db.relationship('Post' , secondary=liks , back_populates='users_liks')
+    posts_disliked = db.relationship('Post' , secondary=liks , back_populates='users_disliks')
+    def __repr__(self):
+        return f'User < {self.id} - {self.email}> '
+    
+    def __init__(self , full_name : str , email : str , password : str , role : int):
+        self.full_name = full_name
+        self.email = email 
+        self.password = password
+        self.role = role
+        
--- mod_user/models.py
+++ mod_user/models.py
@@ -1,3 +1,6 @@
-from sqlalchemy import Column , Integer
+from sqlalchemy import Column , Integer , String 
 from app import db
 
+from mod_blog import models 
+
+# Class User -> mod_blog > models.py
