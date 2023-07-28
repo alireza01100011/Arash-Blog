41b45b4b0300bcf26940bcfe6a43d5331fb06b47
Alireza <armanparsi206@outlook.com>
2023-07-28 20:43:01 +0330

    Add Bcrypt

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from config import config
 
 app = Flask(__name__ , template_folder='Themes')
@@ -10,10 +11,14 @@ db = SQLAlchemy()
 db.init_app(app=app)
 
 migrate = Migrate(app , db)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'user.login'
 login_manager.login_message = 'first Login'
 login_manager.login_message_category = 'info'
+
+becrypt = Bcrypt(app)
+
 from views import *
 
 from mod_admin import admin
