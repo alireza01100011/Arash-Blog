da04c4086e2f88988dfcebed4409028f44ca74d4
Alireza <armanparsi206@outlook.com>
2023-07-28 20:41:46 +0330

    Add Login Maneger

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import config
 
-app = Flask(__name__)
+app = Flask(__name__ , template_folder='Themes')
 app.config.from_object(config)
-
 db = SQLAlchemy()
 db.init_app(app=app)
 
 migrate = Migrate(app , db)
-
+login_manager = LoginManager(app)
+login_manager.login_view = 'user.login'
+login_manager.login_message = 'first Login'
+login_manager.login_message_category = 'info'
 from views import *
 
 from mod_admin import admin
