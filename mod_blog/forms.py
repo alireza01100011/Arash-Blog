d826a61cecad71bae50a1fd9e5ff3966bcb2f685
Alireza <armanparsi206@outlook.com>
2023-07-31 00:52:07 +0330

    Validators PostForm

--- mod_blog/forms.py
+++ mod_blog/forms.py
@@ -1,11 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField , TextAreaField
-from wtforms.validators import DataRequired
-from utils.forms import ListWidget
+from wtforms.validators import DataRequired , ValidationError
+from utils.forms import MultipleCheckboxField
+
+from mod_blog.models import Post
 
 class PostForm(FlaskForm):
     title = StringField( label='Title' , validators=[DataRequired()])
     content = TextAreaField(label='Content' , validators=[DataRequired()])
     summary = StringField(label='Summary' , validators=[])
     slug = StringField('Slug' , validators=[DataRequired()])
-    categories = ListWidget(label='Categories' , coerce=int)
+    categories = MultipleCheckboxField(label='Categories' , coerce=int)
+
+    def validate_title(self , title):
+        post = Post.query.filter(Post.title.ilike(f'{title}')).first()
+        if post :
+            raise ValidationError('This title already exists')
+    
+    def validate_content(self , content):
+        content = Post.query.filter(Post.content.ilike(f'{content}')).first()
+        if content :
+            raise ValidationError('This content already exists')
+        
+    def validate_summary(self , summary):
+        summary = Post.query.filter(Post.summary.ilike(f'{summary}')).first()
+        if summary :
+            raise ValidationError('This summary already exists')
+
+    def validate_slug(self , slug):
+        slug = Post.query.filter(Post.slug.ilike(f'{slug}')).first()
+        if slug :
+            raise ValidationError('This slug already exists')
