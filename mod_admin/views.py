bbf7a680bb5e084bbded044106fb15b10112c3b7
Alireza <armanparsi206@outlook.com>
2023-07-30 20:16:29 +0330

    index Page Admin :)

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,3 +1,9 @@
 from flask import render_template ,  redirect ,  request
-from mod_admin import admin
+from flask_login import login_user , logout_user , login_required , current_user
 
+from mod_admin import admin
+from app import db 
+
+@admin.route('/')
+def index():
+    return render_template('admin/index.html' , title='Dashboard')
