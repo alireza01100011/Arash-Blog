b8ebb3aad50422ba059625afcf8e70566caaa786
Alireza <armanparsi206@outlook.com>
2023-07-31 00:08:28 +0330

    Defining post management func in the admin panel

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -6,6 +6,30 @@ from mod_admin.utils import admin_only_view
 from app import db 
 
 @admin.route('/')
-
 def index():
     return render_template('admin/index.html' , title='Dashboard')
+
+
+#### Post ####
+
+# Show List Post
+@admin.route('posts/')
+def post_show():
+    pass # Show List
+
+# Create Post
+@admin.route('posts/create' , methods=['GET' , 'POST'])
+def post_create():
+    pass # Todo : Create Post
+
+
+# Edite Post
+@admin.route('posts/edite/<int:post_id>' , methods=['GET' , 'POST'])
+def post_edite(post_id):
+    pass # Todo : Edite Post
+
+
+# Delete Post
+@admin.route('posts/delete/<int:post_id>')
+def post_delete(post_id):
+    pass # Todo : Delete Post
