4bbd141d517327ec550d154fedd0a19a01f8d9ea
Alireza <armanparsi206@outlook.com>
2023-07-31 01:04:40 +0330

    Create Post

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,8 +1,9 @@
-from flask import render_template ,  redirect ,  request
+from flask import render_template ,  redirect ,  request , flash
 from flask_login import login_user , logout_user , login_required , current_user
-
+from sqlalchemy.exc import IntegrityError
 from mod_admin import admin
 from mod_blog.models import Post
+from mod_blog.forms import PostForm
 from app import db 
 
 @admin.route('/')
@@ -22,9 +23,33 @@ def post_show():
     # return render_template('admin/posts/post.html' , title='Show Posts' , posts=posts)
 
 # Create Post
-@admin.route('posts/create' , methods=['GET' , 'POST'])
+@admin.route('posts/create/' , methods=['GET' , 'POST'])
 def post_create():
-    pass # Todo : Create Post
+    form = PostForm()
+
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            return render_template('admin/posts/post-form.html' , title=f'Create New Post' , form=form)
+        
+        NewPost = Post(
+            title=form.title.data,
+            content=form.content.data,
+            summary=form.summary.data,
+            slug=form.slug.data,
+            image=1 # -> temporarily until the completion of the blueprint (media)
+        )
+        
+        NewPost.author = current_user
+
+        try :
+            db.session.add(NewPost)
+            db.session.commit()
+            flash('Post successfully created')
+        except IntegrityError :
+            db.session.rollback()
+            flash('Post could not be created successfully')
+
+    return render_template('admin/posts/post-form.html' , title=f'Create New Post' , form=form)
 
 
 # Edite Post
