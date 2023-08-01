608e209486956436d5b0300ec5b1cd7d0b3b334b
Alireza <armanparsi206@outlook.com>
2023-08-01 17:55:08 +0330

    Fix Bug & Validate Password

--- mod_user/froms.py
+++ mod_user/froms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired , Email , EqualTo , ValidationError
 from app import becrypt
 from mod_blog.models import User
 
+import re
 def _get_fields(obj):
     fields = [_ for _ in obj._fields]
     return [getattr(obj , _ ) for _ in fields]
@@ -30,9 +31,16 @@ class RegisterForm(FlaskForm):
     confirm_password = StringField('Confirm Password' , validators=[DataRequired() , EqualTo('password' , message='Password Must Match')])
 
     def validate_email(self , email):
-        _ = User.query.filter(User.email.ilike(f'{email}')).first()
-        if not _ :
+        _ = User.query.filter(User.email.ilike(f'{email.data}')).first()
+        if _ :
             raise ValidationError('This Email Already Exists')
-        
+    
+    def validate_password(self , password):        
+        if not len(password.data) >= 8 :
+            raise ValidationError('The password is shorter than 8 characters')
+             
+        if not re.search("[a-z]", password.data) or not re.search("[A-Z]", password.data) or not re.search("[0-9]", password.data):
+            raise ValidationError('The password is simple, please use a combination of uppercase and lowercase letters and numbers')
+    
     def get_fields(self):
         return _get_fields(self)
