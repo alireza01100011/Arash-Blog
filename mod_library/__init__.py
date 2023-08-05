a443338424376de2bab4e735aaa7d9fe426b49fe
Alireza <armanparsi206@outlook.com>
2023-08-05 18:59:20 +0330

    Set Folder Themplate

--- mod_library/__init__.py
+++ mod_library/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-library = Blueprint('library',__name__,url_prefix='/library/')
+library = Blueprint('library',__name__,url_prefix='/library/' , template_folder='Themes')
 
 from mod_library import views
