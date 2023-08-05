7e1e6e14a0c62cc7a591e55f1f272dbf88a92bc8
Alireza <armanparsi206@outlook.com>
2023-08-05 18:59:34 +0330

    Validator

--- mod_library/forms.py
+++ mod_library/forms.py
@@ -1,15 +1,23 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed , FileRequired
 from wtforms import StringField , FileField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired , ValidationError
 
+from mod_blog.models import File
 class FileForm(FlaskForm):
     name = StringField('Name File' , validators=[DataRequired()])
     discription = StringField('Discription File' , validators=[])
     alt = StringField('Alt File' , validators=[])
     file = FileField('File' , 
                      validators=[
-                         DataRequired() , 
+                         FileRequired() , 
                          FileAllowed(['zip' , 'rar' , 'jpg' , 'jpeg' , 'png' , 'mp3' , 'mp4', 'exe' , 'apk'] , 'Upload File')
                          ])
+    
+    def validate_name(self , name):
+        _ = File.query.filter(File.name.ilike(f'{name.data}')).first()
+        if _ :
+            raise ValidationError('This Name Is Already Used')
+
+    
 
