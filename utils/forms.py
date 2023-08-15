0f4674410620e69080884c17c1188e6ce4ebc4ae
Alireza <armanparsi206@outlook.com>
2023-08-15 20:07:39 +0330

    Move  Formt And _get_fields

--- utils/forms.py
+++ utils/forms.py
@@ -1,6 +1,19 @@
 from wtforms import SelectMultipleField
 from wtforms.widgets import ListWidget , CheckboxInput
 
+formats = {
+    'image' : ['jpg' , 'jpeg' , 'png' , 'webp' , 'gif' , 'svg'],
+    'audio' : ['mp3' , 'oog'  , 'aac' , 'wav'] ,
+    'video' : ['mp4' , 'wmv' , 'webm'],
+    'exe' : ['exe' , 'apk' , 'bash'],
+    'compressed' : ['zip' , 'rar' , 'gz' , 'tar' , '7z'],
+    'document' : ['txt' , 'log' , 'pdf' , 'html' , 'xls' , 'xlsx' , 'odt' , 'PPT' , 'PPTX']
+}
+
 class MultipleCheckboxField(SelectMultipleField):
     widget = ListWidget(prefix_label=False)
     option_widget = CheckboxInput()
+
+def _get_fields(obj):
+    fields = [_ for _ in obj._fields]
+    return [getattr(obj , _ ) for _ in fields]
