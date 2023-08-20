c944172fe8bdbb6c865baed6225a8b5e3852f6ee
Alireza <armanparsi206@outlook.com>
2023-08-20 17:21:53 +0330

    Display the index image (experimental)

--- mod_blog/views.py
+++ mod_blog/views.py
@@ -1,6 +1,6 @@
 from flask import render_template ,  redirect ,  request
 from mod_blog import blog
-from mod_blog.models import Post
+from mod_blog.models import Post , Madie
 
 @blog.route('/')
 def index():
@@ -15,4 +15,6 @@ def author():
 @blog.route('post/<string:slug>')
 def post(slug):
     post = Post.query.filter(Post.slug == slug).first()
-    return render_template('blog/post.html' , post=post , title=post.title)
+    img =  Madie.query.get(post.image).filename
+
+    return render_template('blog/post.html' , post=post , title=post.title , img=img)
