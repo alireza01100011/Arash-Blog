8e01c3db6361a5f0bcb8affc63a195529b5031dc
Alireza Mosaveyan <Armanparsi206@outlook.com>
2023-09-04 22:05:41 +0330

    Add Button Seve Post

--- static/blog/assets/js/like_dislike.js
+++ static/blog/assets/js/like_dislike.js
@@ -22,6 +22,20 @@ function color_defult(){
     document.getElementById("dislike-i").style.backgroundColor = '#ffff';
 }
 
+function color_save(x){
+    if (x == 'active') {
+      
+      document.getElementById("save-a").style.color = '#ffff';
+      document.getElementById("save-i").style.backgroundColor = 'black';
+
+    }
+    else {
+      document.getElementById("save-a").style.color = 'black';
+      document.getElementById("save-i").style.backgroundColor = '#ffff';
+    };
+
+}
+
 function like(){
     const xhr = new XMLHttpRequest();
     var post_id = document.getElementById('like-a').getAttribute('value')
@@ -70,4 +84,29 @@ function dislike(){
         Swal.fire("" , 'The post was not liked successfully');
       }
     };
+}
+
+function save(){
+  const xhr = new XMLHttpRequest();
+  var post_id = document.getElementById('save-a').getAttribute('value')
+  xhr.open("POST", "http://127.0.0.1:5000/blog/posts/save/" + post_id);
+  xhr.send();
+  xhr.responseType = "json";
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      const data = xhr.response;
+      if (data == 0){
+        Swal.fire("" , 'The post has been successfully saved');
+        color_save('active');
+      }
+      else{
+        Swal.fire("" , 'Removed from saved posts');
+        color_save('diactive');
+      }
+
+    } else {
+      console.log(`Error: ${xhr.status}`);
+      Swal.fire("" , 'The post was not saved successfully');
+    }
+  };
 }
