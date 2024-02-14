8399f53f1068b49b6f9dfe91408a5b9b335ada25
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-02-14 16:47:34 +0330

    Fix Error

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import json
 
 class config():
-    SECRET_KEY = os.urandom('64')
+    SECRET_KEY = os.urandom(64)
 
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     REDIS_SERVER_URL = os.getenv('REDIS_SERVER_URL')
@@ -11,6 +11,8 @@ class config():
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_PORT = os.getenv('MAIL_PORT')
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+
+    SERVER_NAME_MAIL = os.getenv('SERVER_NAME_MAIL') 
 
 class site():
     with open(os.path.join('' , 'site.json') , 'r') as file:
