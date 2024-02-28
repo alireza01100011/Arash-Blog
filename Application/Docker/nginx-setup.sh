4ada561f41792fd897ceab0453d02122e5179b33
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-02-28 16:12:03 +0330

    update Nginx config

--- Application/Docker/nginx-setup.sh
+++ Application/Docker/nginx-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 
+# Create Config File
 envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < /etc/nginx/org.nginx.conf.template \
     > /etc/nginx/nginx.conf
 
+# Start Nginx
 nginx -g 'daemon off;'
